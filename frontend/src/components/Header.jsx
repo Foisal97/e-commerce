8afefa7e0b5e761f519ad85314c89ca3ffa4015f
Foisal97 <36104702+Foisal97@users.noreply.github.com>
2024-11-09 20:44:01 +0600

    list of products

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,22 +1,28 @@
-import { Navbar, Nav, Container } from 'react-bootstrap'
-import { FaShoppingCart, FaUser} from 'react-icons/fa'
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
 const Header = () => {
   return (
     <header>
-        <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
-            <Container>
-                <Navbar.Brand href="/">Proshop</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                <Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">Proshop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
 
-                <Nav className="ms-auto">
-                    <Nav.Link href="/cart"><FaShoppingCart/>Cart</Nav.Link>
-                    <Nav.Link href="/login"><FaUser/>Sign-In</Nav.Link>
-                </Nav>
-            </Container>
-        </Navbar>
+          <Nav className="ms-auto">
+            <Nav.Link href="/cart">
+              <FaShoppingCart />
+              Cart
+            </Nav.Link>
+            <Nav.Link href="/login">
+              <FaUser />
+              Sign-In
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
