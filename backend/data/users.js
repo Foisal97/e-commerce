e20eb9ad6cdd0f2ffe3320cfd3bbeceaf023d2a3
Foisal97 <36104702+Foisal97@users.noreply.github.com>
2024-11-17 14:05:47 +0600

    data modeling and get data from mongoDB

---
+++ backend/data/users.js
@@ -0,0 +1,23 @@
+
+const user = [
+    {
+        name: "Admin User",
+        email: "admin@email.com",
+        password: 123456,
+        isAdmin: true
+    },
+    {
+        name: "John Doe",
+        email: "john@email.com",
+        password: 123456,
+        isAdmin: false
+    }, 
+    {
+        name: "Jane Doe",
+        email: "jane@email.com",
+        password: 123456,
+        isAdmin: true
+    }
+];
+
+export default user;
