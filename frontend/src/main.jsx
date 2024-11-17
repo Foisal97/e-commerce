1b3b5c867936ff4e966f29961debed03312fb3d9
Foisal97 <36104702+Foisal97@users.noreply.github.com>
2024-11-17 21:25:16 +0600

    get all products using redux

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import store from './store.js';
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -24,6 +26,8 @@ const router = createBrowserRouter(
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}>
     <RouterProvider router={router}></RouterProvider>
+    </Provider>
   </StrictMode>,
 )
