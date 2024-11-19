9ca15e57ca6275d0006d114e34d95daf1efe6340
Foisal97 <36104702+Foisal97@users.noreply.github.com>
2024-11-19 23:12:48 +0600

    remove from cart functionality

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -11,7 +11,7 @@ import {
 } from "react-bootstrap";
 import { FaTrash } from "react-icons/fa";
 import Message from "../components/Message";
-import { addToCart } from "../slices/cartSlice";
+import { addToCart, removeFromCart } from "../slices/cartSlice";
 
 
 const CartScreen = () => {
@@ -23,6 +23,15 @@ const CartScreen = () => {
   const addToCartHandler = async(product, qty) =>{
     dispatch(addToCart({...product, qty}))
   }
+
+  const removeFromCartHandler = async(id) =>{
+    dispatch(removeFromCart(id))
+  }
+
+  const checkoutHandler = () => {
+    navigate("/login?redirect=/shipping");
+  }
+
   return (
     <Row>
       <Col md={8}>
@@ -57,7 +66,7 @@ const CartScreen = () => {
                     </Form.Control>
                   </Col>
                   <Col md={2}>
-                  <Button type="button" varriant="light">
+                  <Button type="button" varriant="light" onClick={()=> removeFromCartHandler(item._id)}>
                     <FaTrash></FaTrash>
                   </Button>
                   </Col>
@@ -75,7 +84,7 @@ const CartScreen = () => {
                 ${cartItems.reduce((acc, item)=> acc+ item.qty * item.price, 0).toFixed(2)}
             </ListGroup.Item>
             <ListGroup.Item>
-                <Button type="button" className="btn-block" disabled={ cartItems.length === 0}>
+                <Button type="button" className="btn-block" disabled={ cartItems.length === 0} onClick={checkoutHandler}>
                     Proceed to Checkout
                 </Button>
             </ListGroup.Item>
