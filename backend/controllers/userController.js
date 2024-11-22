8343a1643341f446ffbcf7bedf1ccfedba5f2296
Foisal97 <36104702+Foisal97@users.noreply.github.com>
2024-11-22 22:06:34 +0600

    profile endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -77,7 +77,19 @@ const logoutUser = asyncHandler(async(req, res)=>{
 //@access Private
 
 const getUserProfile = asyncHandler(async(req, res)=>{
-    res.send("user profile");
+    const user = await User.findById(req.user._id);
+    if(user){
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            password: user.password
+        });
+    }
+    else{
+        res.status(404);
+        throw new Error("user not found")
+    }
 })
 
 
@@ -86,7 +98,25 @@ const getUserProfile = asyncHandler(async(req, res)=>{
 //@access Private
 
 const updateUserProfile = asyncHandler(async(req, res)=>{
-    res.send("update user");
+    const user = await User.findById(req.user._id);
+    if(user){
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+        if(req.body.password){
+            user.password = req.body.password;
+        }
+    const updateUser = await user.save();
+    res.status(200).json({
+        _id: updateUser._id,
+        name: updateUser.name,
+        email: updateUser.email,
+        isAdmin: updateUser.isAdmin,
+    })
+    }
+    else{
+        res.status(404);
+        throw new Error("User not found");
+    }
 })
 
 
